87d0a2c269936ed436f33ab51d916e98030af166
Dmitry <flymedllva@gmail.com>
2019-07-09 00:09:05 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# Picture to text
+# Переводит фотографии в текст
+Использовать преимущественно для фотографий кода ¯\\\_(ツ)\_/¯
+Хотя, можно переделать
+
+У функции несколько параметров:
+* img - путь к картинке. (например: 1.png)
+* spaces - вид табуляции или пробелов (по умолчанию 1 таб)
+* text - текст в начале того, что считается с картинки
+
+Зависимости:
+```python
+from PIL import Image
+from tesserocr import PyTessBaseAPI, RIL, image_to_text
+import locale
+```
+Нужно для tesserocr в некоторых языках, причем поменять нужно до импорта tesserocr
+```python
+locale.setlocale(locale.LC_ALL, 'C')
+```
+
+# Translates photos into text
+Use primarily for code photos ¯\\\_(ツ)\_/¯
+Although, it is possible to alter
+
+The function has several parameters:
+* img - the path to the picture (for example: 1.png)
+* spaces - tab or space view (default is 1 tab)
+* text - text at the beginning of what counts from the picture
+
+Dependences:
+```python
+from PIL import Image
+from tesserocr import PyTessBaseAPI, RIL, image_to_text
+import locale
+```
+Need for tesserocr in some languages, and you need to change before importing tesseract
+```python
+locale.setlocale(locale.LC_ALL, 'C')
+```
---
+++ picture-to-text.py
@@ -0,0 +1,36 @@
+def picture_to_text(img, spaces="    ", text=""):
+    """
+    Translate image to text with tabulation
+
+    Keyword arguments:
+    img -- the path to the picture (example: 1.png)
+    spaces -- type of indentation between lines (default: '    ')
+    text -- text at the beginning of lines (default: '')
+
+    :return: the text from the image
+
+    """
+    import locale
+    locale.setlocale(locale.LC_ALL, 'C')
+    from PIL import Image
+    from tesserocr import PyTessBaseAPI, RIL, image_to_text
+    image = Image.open(img)
+    areas = {}
+    with PyTessBaseAPI() as api:
+        api.SetImage(image)
+        for i, (im, box, _, _) in enumerate(api.GetComponentImages(RIL.TEXTLINE, True)):
+            api.SetRectangle(box['x'], box['y'], box['w'], box['h'])
+            areas[i] = {"line": api.GetUTF8Text().strip(), "confidence": api.MeanTextConf(), "x": box['x'], "y": box['y'], "width": box['w'], "heigh": box['h']}
+    min_x = min(areas.values(), key=lambda item: item['x'])['x']
+    areas = {key: {"line": values["line"],
+                    "confidence": values["confidence"],
+                    "x": values["x"] - min_x,
+                    "y": values["y"],
+                    "width": values["width"],
+                    "heigh": values["heigh"]} for key, values in areas.items()}
+    if len(areas) > 1:
+        min_x = sorted(areas.values(), key=lambda item: item['x'])[1]['x']
+    else:
+        min_x = min(areas.values(), key=lambda item: item['x'])['x']
+    return "\n".join([f"{spaces * item['x']}{item['line']}" for item in {key: {"line": values["line"], "confidence": values["confidence"], "x": int(values["x"] / min_x), "y": values["y"], "width": values["width"], "heigh": values["heigh"]} for key, values in areas.items()}.values()])
+
